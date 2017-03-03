5a856d13f532b95ba68e1b115bfef6e7e6667857
emilMircea <emil.mircea30@gmail.com>
2017-03-03 15:57:24 +0100

    initial commit

---
+++ app/views/api/v1/contacts/index.json.jbuilder
@@ -0,0 +1,7 @@
+json.name @contacts.name
+json.email @contacts.email
+
+json.current do
+  json.name
+  json.email
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      resources :contacts
+    end
+  end
+  resources :contacts
+
+end
